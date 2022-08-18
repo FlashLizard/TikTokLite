92a1b7f092e29265e108697e1b6b52560d34587d
FlashLizard <1256854232@qq.com>
2022-08-18 23:47:15 +0800

    add rank film, tvplay, variety item xml

--- app/src/main/java/com/qxy/tiktoklite/MainActivity.java
+++ app/src/main/java/com/qxy/tiktoklite/MainActivity.java
@@ -54,73 +54,10 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
 
-    DouYinOpenApi douYinOpenApi;
-    OkHttpClient client = new OkHttpClient();
-
-    public static String code = "null";
-
-    private String mScope = "user_info";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        douYinOpenApi=DouYinOpenApiFactory.create(this);
-
-        findViewById(R.id.get_token).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                testOkHttp();
-            }
-        });
-        sendAuth();
-    }
-
-    private void testOkHttp(){
-        //RequestBody body = RequestBody.create(MediaType.parse("text/plain;charset=utf-8"),"");
-
-        FormBody body = new FormBody.Builder()
-                .add("client_secret","a68efe35e5b4b2c136a2149d38f48871")
-                .add("code",code)
-                .add("grant_type","authorization_code")
-                .add("client_key","aw9uvum16m62angi")
-                .build();
-
-        Request request = new Request.Builder()
-                .post(body)
-                .url("https:open.douyin.com"+"/oauth/access_token/")
-                .build();
-
-        Call call = client.newCall(request);
-
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Toast.makeText(MainActivity.this, "Post Failed", Toast.LENGTH_LONG);
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String str = response.body().string();
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((TextView)findViewById(R.id.response_str)).setText(str);
-                    }
-                });
-            }
-        });
-    }
-
-    private boolean sendAuth() {
-        Authorization.Request request = new Authorization.Request();
-        request.scope = mScope;                          // 用户授权时必选权限
-        request.optionalScope0 = "mobile";     // 用户授权时可选权限（默认选择）
-//        request.optionalScope0 = mOptionalScope1;    // 用户授权时可选权限（默认不选）
-        request.state = "ww";                                   // 用于保持请求和回调的状态，授权请求后原样带回给第三方。
-        return douYinOpenApi.authorize(request);               // 优先使用抖音app进行授权，如果抖音app因版本或者其他原因无法授权，则使用wap页授权
-
     }
 
 }
